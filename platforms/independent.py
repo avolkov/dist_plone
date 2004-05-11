3d03af1d80f860446af9351f13fe7c1287d771af
Andy McKay <amckay@mozilla.com>
2004-05-11 17:50:25 +0000

    typo

--- platforms/independent.py
+++ platforms/independent.py
@@ -17,7 +17,7 @@ PLONE_CORE = [
            {'CMFActionIcons': ZProduct,}
     ),
     ZProduct('CMFFormController', 'http://heanet.dl.sourceforge.net/sourceforge/collective/CMFFormController-1.0.2.tgz'),
-    ZProduct('CMFPlone', 'http://osdn.dl.sourceforge.net/sourceforge/plone/PloneBase-2.0.2.tgz'),
+    ZProduct('CMFPlone', 'http://osdn.dl.sourceforge.net/sourceforge/plone/PloneBase-2.0.2.tar.gz'),
     ZProduct('CMFQuickInstallerTool', 'http://heanet.dl.sourceforge.net/sourceforge/collective/CMFQuickInstallerTool-1.5.0.tgz'),
     ZProduct('Formulator', 'http://zope.org/Members/infrae/Formulator/Formulator-1.6.2/Formulator-1.6.2.tgz'),
     ZProduct('GroupUserFolder', 'http://heanet.dl.sourceforge.net/sourceforge/collective/GroupUserFolder-2.0.1.tgz'),
