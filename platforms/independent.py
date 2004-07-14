c54f6329826234022d010e1dc5d0e9b4c2f6a6bd
Christian Heimes <tiran@cheimes.de>
2004-07-14 23:28:30 +0000

    Moved to new CMF location and CMF 1.4.5 Moved to new SecureMailHost version Moved to new PlacelessTranslationService Location Added kupu 1.1 (but commented out the installation) Moved to python 2.3.4 Moved to Zope 2.7.1 Added Hotfix for Zope 2.7.1 Seperated ADDONS and Archetypes to ADDONS and AT1_2 Added future support for AT 1.3 and ATContentTypes (currently not in use) Moved to ExternalEdit 0.8 (has some required fixed for Zope 2.7) Moved base url for sourceforge download server to a var That is all :)

--- platforms/independent.py
+++ platforms/independent.py
@@ -1,9 +1,14 @@
 from dist_plone import Software, PyModule, ZProduct, Bundle
 
+BASE = 'http://voxel.dl.sourceforge.net/sourceforge/'
+PLONE_BASE = BASE + 'plone/'
+PLONE_I18N_BASE = BASE + 'plone-i18n/'
+COLLECTIVE_BASE = BASE + 'collective/'
+ARCHETYPES_BASE = BASE + 'archetypes/'
 
 PLONE_CORE = [
     Bundle('CMF',
-           'http://cmf.zope.org/download/CMF-1.4.4/CMF-1.4.4.tar.gz',
+           'http://zope.org/Products/CMF/CMF-1.4.5/CMF-1.4.5.tar.gz',
            { 'CMFCalendar': ZProduct,
              'CMFCore'    : ZProduct,
              'CMFDefault' : ZProduct,
@@ -16,35 +21,54 @@ PLONE_CORE = [
            'http://zope.org/Members/tseaver/CMFActionIcons/CMFActionIcons-0.9/CMFActionIcons-0.9.tar.gz',
            {'CMFActionIcons': ZProduct,}
     ),
-    ZProduct('CMFFormController', 'http://osdn.dl.sourceforge.net/sourceforge/collective/CMFFormController-1.0.3-beta.tar.gz'),
-    ZProduct('CMFPlone', 'http://osdn.dl.sourceforge.net/sourceforge/plone/PloneBase-2.0.3.tar.gz'),
-    ZProduct('CMFQuickInstallerTool', 'http://osdn.dl.sourceforge.net/sourceforge/collective/CMFQuickInstallerTool-1.5.0.tgz'),
+    ZProduct('CMFFormController', COLLECTIVE_BASE + 'CMFFormController-1.0.3-beta.tar.gz'),
+    ZProduct('CMFPlone', PLONE_BASE + 'PloneBase-2.0.3.tar.gz'),
+    ZProduct('CMFQuickInstallerTool', COLLECTIVE_BASE + 'CMFQuickInstallerTool-1.5.0.tgz'),
     ZProduct('Formulator', 'http://zope.org/Members/infrae/Formulator/Formulator-1.6.2/Formulator-1.6.2.tgz'),
-    ZProduct('GroupUserFolder', 'http://osdn.dl.sourceforge.net/sourceforge/collective/GroupUserFolder-2.0.1.tgz'),
-    ZProduct('PlacelessTranslationService', 'http://osdn.dl.sourceforge.net/sourceforge/collective/PlacelessTranslationService-1.0fork-rc7.tar.gz'),
-    ZProduct('PloneErrorReporting', 'http://osdn.dl.sourceforge.net/sourceforge/collective/PloneErrorReporting-0.1.tar.gz'),
-    ZProduct('PloneTranslations', 'http://osdn.dl.sourceforge.net/sourceforge/plone-i18n/PloneTranslations-0.4.tar.gz'),
-    ZProduct('SecureMailHost', 'http://unc.dl.sourceforge.net/sourceforge/collective/SecureMailHost-0.2beta2.tar.gz'),
+    ZProduct('GroupUserFolder', COLLECTIVE_BASE + 'GroupUserFolder-2.0.1.tgz'),
+    ZProduct('PlacelessTranslationService', COLLECTIVE_BASE + 'PlacelessTranslationService-1.0-rc8.tar.gz'),
+    ZProduct('PloneErrorReporting', COLLECTIVE_BASE + 'PloneErrorReporting-0.1.tar.gz'),
+    ZProduct('PloneTranslations', PLONE_I18N_BASE + 'PloneTranslations-0.4.tar.gz'),
+    ZProduct('SecureMailHost', COLLECTIVE_BASE + 'SecureMailHost-0.2rc1.tar.gz'),
+    # XXX hotfix is required for 2.7.0 and 2.7.1
+    ZProduct('Hotfix_20040714', 'http://zope.org/Products/Zope/Hotfix_2004-07-14/Zope%202.7.0%20-%202.7.1/Hotfix_2004-07-14.tar.gz'),
     ]
 
 ADDONS = [
-    Bundle('Archetypes',  'http://osdn.dl.sourceforge.net/sourceforge/archetypes/archetypes-1.2.5-rc4.tgz',
+    ZProduct('ExternalEditor', 'http://zope.org/Members/Caseman/ExternalEditor/0.8/ExternalEditor-0.8-src.tgz'),
+    ZProduct('Epoz', 'http://mjablonski.zope.de/Epoz/releases/Epoz-0.8.0.tar.gz'),
+##    ZProduct('kupu', 'http://kupu.oscom.org/midcom-serveattachmentguid-6799a2e8aec0edc19a6a1f2682ac8a4a/kupu-1.1.tgz')
+    ]
+
+AT1_2 = [
+    Bundle('Archetypes',  ARCHETYPES_BASE + 'archetypes-1.2.5-rc4.tgz',
            { 'Archetypes': ZProduct,
              'generator' : ZProduct,
              'validation': ZProduct,
            }
     ),
     Bundle('PortalTransforms',
-           'http://osdn.dl.sourceforge.net/sourceforge/archetypes/PortalTransforms-1.0.4.tgz',
+           ARCHETYPES_BASE + 'PortalTransforms-1.0.4.tgz',
            {'PortalTransforms': ZProduct,}
+
     ),
-    ZProduct('ExternalEditor', 'http://zope.org/Members/Caseman/ExternalEditor/0.7.2/ExternalEditor-0.7.2-src.tgz'),
-    ZProduct('Epoz', 'http://mjablonski.zope.de/Epoz/releases/Epoz-0.8.0.tar.gz'),
     ]
+
+AT1_3 = [
+    Bundle('Archetypes',  ARCHETYPES_BASE + 'archetypes-1.3.0_b3.tgz',
+           { 'Archetypes': ZProduct,
+             'generator' : ZProduct,
+             'validation': ZProduct,
+             'PortalTransforms': ZProduct,
+             'MimetypesRegistry': ZProduct,
+           }
+    ),
+    ZProduct('ATContentTypes', COLLECTIVE_BASE + 'ATCT-0.2beta8.tar.gz'),
+]
+
 
 README_TXT = """Plone's README is in CMFPlone/README.txt
 """
-
 
 
 class Distribution:
@@ -52,14 +76,15 @@ class Distribution:
     target = 'independent'
 
     # this is what plone is based on
-    python =  Software('python', 'http://python.org/ftp/python/2.3.3/Python-2.3.3.tgz')
-    zope   =  Software('zope'  , 'http://zope.org/Products/Zope/2.7.0/Zope-2.7.0.tgz')
+    python =  Software('python', 'http://python.org/ftp/python/2.3.4/Python-2.3.4.tgz')
+    # XXX remove hotfix after moving to 2.7.2
+    zope   =  Software('zope'  , 'http://zope.org/Products/Zope/2.7.1/Zope-2.7.1.tgz')
 
     # plone core
     core   = PLONE_CORE
 
     # plone addons
-    addons = ADDONS
+    addons = ADDONS + AT1_2
 
     # the readme.txt
     readme = README_TXT
