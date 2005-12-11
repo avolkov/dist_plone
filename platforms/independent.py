934e9460d762e7d961b2d0351a173c0bb5ea5e6e
Hanno Schlichting <hanno@hannosch.eu>
2005-12-11 04:30:55 +0000

    Added the last products from sf collective / plone-i18n to plone.org. Plone itself is the last remaining product hosted on sf.net :)

--- platforms/independent.py
+++ platforms/independent.py
@@ -5,11 +5,6 @@ BASE = 'http://osdn.dl.sourceforge.net/sourceforge/'
 #BASE = 'http://switch.dl.sourceforge.net/sourceforge/'
 
 PLONE_BASE = BASE + 'plone/'
-#PLONE_BASE = 'http://dev.clearwind.ca/Files/Plone/Nightly/'
-#PLONE_BASE = 'http://localhost/plone21/'
-
-PLONE_I18N_BASE = BASE + 'plone-i18n/'
-COLLECTIVE_BASE = BASE + 'collective/'
 
 PLONE_ORG = 'http://plone.org/products/'
 OSCOM_ORG = 'http://kupu.oscom.org/'
@@ -28,14 +23,14 @@ PLONE_CORE = [
            }
     ),
     ZProduct('CMFPlone', PLONE_BASE + 'PloneBase-2.1.1.tar.gz'),
-    ZProduct('CMFFormController', COLLECTIVE_BASE + 'CMFFormController-1.0.6.tar.gz'),
-    ZProduct('CMFQuickInstallerTool', COLLECTIVE_BASE + 'CMFQuickInstallerTool-1.5.5.tgz'),
+    ZProduct('CMFFormController', PLONE_ORG + 'cmfformcontroller/releases/1.0.6/CMFFormController-1.0.6.tar.gz'),
+    ZProduct('CMFQuickInstallerTool', PLONE_ORG + 'cmfquickinstallertool/releases/1.5.5/CMFQuickInstallerTool-1.5.5.tgz'),
     #XXX ZProduct('BTreeFolder2', 'http://hathawaymix.org/Software/BTreeFolder2/BTreeFolder2-1.0.1.tar.gz'),
     ZProduct('BTreeFolder2', 'http://plone.org/Members/tiran/BTreeFolder2-1.0.2.tar.gz'),
     ZProduct('GroupUserFolder', PLONE_ORG + 'groupuserfolder/releases/3.4/GroupUserFolder-3.4.tar.gz'),
     ZProduct('PlacelessTranslationService', PLONE_ORG + 'pts/releases/1.2.5/PlacelessTranslationService-1.2.5.tar.gz'),
-    ZProduct('PloneErrorReporting', COLLECTIVE_BASE + 'PloneErrorReporting-0.11.tar.gz'),
-    ZProduct('PloneTranslations', PLONE_I18N_BASE + 'PloneTranslations-2.1.1.tar.gz'),
+    ZProduct('PloneErrorReporting', PLONE_ORG + 'ploneerrorreporting/releases/0.11/PloneErrorReporting-0.11.tar.gz'),
+    ZProduct('PloneTranslations', PLONE_ORG + 'plonetranslations/releases/2.1.1/PloneTranslations-2.1.1.tar.gz'),
     ZProduct('PloneLanguageTool', PLONE_ORG + 'plonelanguagetool/releases/0.8/PloneLanguageTool-0.8.tar.gz'),
     ZProduct('SecureMailHost', PLONE_ORG + 'smh/releases/1.0.1/SecureMailHost-1.0.1.tar.gz'),
     ZProduct('ExtendedPathIndex', PLONE_ORG + 'extendedpathindex/releases/2.2/ExtendedPathIndex-2.2.tar.gz'),
@@ -47,8 +42,6 @@ PLONE_CORE = [
 
 ADDONS = [
     ZProduct('ExternalEditor', 'http://plope.com/software/ExternalEditor/ExternalEditor-0.9.1-src.tgz'),
-    #ZProduct('Epoz', 'http://mjablonski.zope.de/Epoz/releases/Epoz-2.0.0.tar.gz'),
-    #XXX ZProduct('kupu', 'http://kupu.oscom.org/midcom-serveattachmentguid-6b4584286e6cd5bbf2f82f0a83ead0e2/kupu-1.2.1.tar.gz')
     ZProduct('kupu', OSCOM_ORG + 'midcom-serveattachmentguid-f40122579e491f7a7417987bef0c49ee/kupu-1.3.1.tar.gz')
 ]
 
