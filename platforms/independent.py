7aa9918dbce67e1e196bcb15c7e6b903b103dd1b
Stefan H. Holek <stefan@epy.co.at>
2006-01-20 20:18:39 +0000

    Update to Plone 2.1.2.

--- platforms/independent.py
+++ platforms/independent.py
@@ -23,7 +23,7 @@ PLONE_CORE = [
              'DCWorkflow' : ZProduct,
            }
     ),
-    ZProduct('CMFPlone', PLONE_BASE + 'PloneBase-2.1.2-rc2.tar.gz'),
+    ZProduct('CMFPlone', PLONE_BASE + 'PloneBase-2.1.2.tar.gz'),
     ZProduct('CMFFormController', PLONE_ORG + 'cmfformcontroller/releases/1.0.7/CMFFormController-1.0.7.tar.gz'),
     ZProduct('CMFQuickInstallerTool', PLONE_ORG + 'cmfquickinstallertool/releases/1.5.7/CMFQuickInstallerTool-1.5.7.tar.gz'),
     #XXX ZProduct('BTreeFolder2', 'http://hathawaymix.org/Software/BTreeFolder2/BTreeFolder2-1.0.1.tar.gz'),
