2af149197b3ea1dde2fd9c3ecf859d154687d62e
Stefan H. Holek <stefan@epy.co.at>
2006-05-12 13:49:25 +0000

    Update to tarball versions existing today. Will need further updates for 2.1.3 obviously.

--- platforms/independent.py
+++ platforms/independent.py
@@ -12,7 +12,7 @@ OSCOM_ORG = 'http://kupu.oscom.org/'
 
 PLONE_CORE = [
     Bundle('CMF',
-           'http://www.zope.org/Products/CMF/CMF-1.5.5/CMF-1.5.5.tar.gz',
+           'http://www.zope.org/Products/CMF/CMF-1.5.6/CMF-1.5.6.tar.gz',
            { 'CMFActionIcons': ZProduct,
              'CMFCalendar': ZProduct,
              'CMFCore'    : ZProduct,
@@ -32,13 +32,13 @@ PLONE_CORE = [
     ZProduct('PlacelessTranslationService', PLONE_ORG + 'pts/releases/1.2.5/PlacelessTranslationService-1.2.5.tar.gz'),
     ZProduct('PloneErrorReporting', PLONE_ORG + 'ploneerrorreporting/releases/0.11/PloneErrorReporting-0.11.tar.gz'),
     ZProduct('PloneTranslations', PLONE_ORG + 'plonetranslations/releases/2.1.2/PloneTranslations-2.1.2.tar.gz'),
-    ZProduct('PloneLanguageTool', PLONE_ORG + 'plonelanguagetool/releases/0.9/PloneLanguageTool-0.9.tar.gz'),
-    ZProduct('SecureMailHost', PLONE_ORG + 'securemailhost/releases/1.0.2/SecureMailHost-1.0.2.tar.gz'),
+    ZProduct('PloneLanguageTool', PLONE_ORG + 'plonelanguagetool/releases/1.0/PloneLanguageTool-1.0.tar.gz'),
+    ZProduct('SecureMailHost', PLONE_ORG + 'securemailhost/releases/1.0.3/SecureMailHost-1.0.3.tar.gz'),
     ZProduct('ExtendedPathIndex', PLONE_ORG + 'extendedpathindex/releases/2.3/ExtendedPathIndex-2.3.tar.gz'),
-    ZProduct('ResourceRegistries', PLONE_ORG + 'resourceregistries/releases/1.1/resourceregistries-1-1-tar.gz'),
+    ZProduct('ResourceRegistries', PLONE_ORG + 'resourceregistries/releases/1.2.1/resourceregistries-1-2-1-tar.gz'),
     ZProduct('ATReferenceBrowserWidget', PLONE_ORG + 'atreferencebrowserwidget/releases/1.2/atreferencebrowserwidget-1_2.tgz'),
     ZProduct('ATContentTypes', PLONE_ORG + 'atcontenttypes/releases/1.0.3/ATContentTypes-1.0.3.tar.gz'),
-    ZProduct('CMFDynamicViewFTI', PLONE_ORG + 'cmfdynamicviewfti/releases/1.0.2/CMFDynamicViewFTI-1.0.2.tar.gz'),
+    ZProduct('CMFDynamicViewFTI', PLONE_ORG + 'cmfdynamicviewfti/releases/1.0.4/CMFDynamicViewFTI-1.0.4.tar.gz'),
 ]
 
 ADDONS = [
@@ -48,7 +48,7 @@ ADDONS = [
 ]
 
 AT1_3 = [
-    ZProduct('Archetypes', PLONE_ORG + 'archetypes/releases/1.3.7-final/archetypes-1.3.7-final-bundle.tar.gz'),
+    ZProduct('Archetypes', PLONE_ORG + 'archetypes/releases/1.3.8-final/archetypes-1.3.8-final-bundle.tar.gz'),
 ]
 
 README_TXT = """Plone's README is in CMFPlone/README.txt
@@ -60,7 +60,7 @@ class Distribution:
 
     # this is what plone is based on
     python =  Software('python', 'http://python.org/ftp/python/2.3.5/Python-2.3.5.tgz')
-    zope   =  Software('zope'  , 'http://www.zope.org/Products/Zope/2.8.5/Zope-2.8.5-final.tgz')
+    zope   =  Software('zope'  , 'http://www.zope.org/Products/Zope/2.8.6/Zope-2.8.6-final.tgz')
 
     # plone core
     core   = PLONE_CORE
