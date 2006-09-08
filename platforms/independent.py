403b57286fbeb44093d2956c8a5a7931c22ff964
Stefan H. Holek <stefan@epy.co.at>
2006-09-08 21:15:59 +0000

    Bump up Zope, CMF, GRUF and RR.

--- platforms/independent.py
+++ platforms/independent.py
@@ -10,7 +10,7 @@ OSCOM_ORG = 'http://kupu.oscom.org/'
 
 PLONE_CORE = [
     Bundle('CMF',
-           'http://www.zope.org/Products/CMF/CMF-1.5.6/CMF-1.5.6.tar.gz',
+           'http://www.zope.org/Products/CMF/CMF-1.5.7/CMF-1.5.7.tar.gz',
            { 'CMFActionIcons': ZProduct,
              'CMFCalendar': ZProduct,
              'CMFCore'    : ZProduct,
@@ -26,14 +26,14 @@ PLONE_CORE = [
     ZProduct('CMFQuickInstallerTool', PLONE_ORG + 'cmfquickinstallertool/releases/1.5.9/CMFQuickInstallerTool-1.5.9.tar.gz'),
     #XXX ZProduct('BTreeFolder2', 'http://hathawaymix.org/Software/BTreeFolder2/BTreeFolder2-1.0.1.tar.gz'),
     ZProduct('BTreeFolder2', 'http://cvs.zope.org/Products/BTreeFolder2/BTreeFolder2.tar.gz?tarball=1&only_with_tag=BTreeFolder2-1_0_2', archive_rename='BTreeFolder2-1.0.2.tar.gz'),
-    ZProduct('GroupUserFolder', PLONE_ORG + 'groupuserfolder/releases/3.52/groupuserfolder-3-52.tgz'),
+    ZProduct('GroupUserFolder', PLONE_ORG + 'groupuserfolder/releases/3.53/GroupUserFolder-3.53.tgz'),
     ZProduct('PlacelessTranslationService', PLONE_ORG + 'pts/releases/1.3.1/PlacelessTranslationService-1.3.1.tar.gz'),
     ZProduct('PloneErrorReporting', PLONE_ORG + 'ploneerrorreporting/releases/0.11/PloneErrorReporting-0.11.tar.gz'),
     ZProduct('PloneTranslations', PLONE_ORG + 'plonetranslations/releases/2.1.3/PloneTranslations-2.1.3-final.tar.gz'),
     ZProduct('PloneLanguageTool', PLONE_ORG + 'plonelanguagetool/releases/1.3/PloneLanguageTool-1.3.tar.gz'),
     ZProduct('SecureMailHost', PLONE_ORG + 'securemailhost/releases/1.0.4/SecureMailHost-1.0.4.tar.gz'),
     ZProduct('ExtendedPathIndex', PLONE_ORG + 'extendedpathindex/releases/2.3/ExtendedPathIndex-2.3.tar.gz'),
-    ZProduct('ResourceRegistries', PLONE_ORG + '/resourceregistries/releases/1.2.2/resourceregistries-1-2-2-tar.gz'),
+    ZProduct('ResourceRegistries', PLONE_ORG + '/resourceregistries/releases/1.2.3/ResourceRegistries-1.2.3.tar.gz'),
     ZProduct('ATReferenceBrowserWidget', PLONE_ORG + 'atreferencebrowserwidget/releases/1.4/ATReferenceBrowserWidget-1.4.tar.gz'),
     ZProduct('ATContentTypes', PLONE_ORG + 'atcontenttypes/releases/1.0.5/ATContentTypes-1.0.5.tar.gz'),
     ZProduct('CMFDynamicViewFTI', PLONE_ORG + 'cmfdynamicviewfti/releases/1.0.4/CMFDynamicViewFTI-1.0.4.tar.gz'),
@@ -58,7 +58,7 @@ class Distribution:
 
     # this is what plone is based on
     python =  Software('python', 'http://python.org/ftp/python/2.3.5/Python-2.3.5.tgz')
-    zope   =  Software('zope'  , 'http://www.zope.org/Products/Zope/2.8.7/Zope-2.8.7-final.tgz')
+    zope   =  Software('zope'  , 'http://www.zope.org/Products/Zope/2.8.8/Zope-2.8.8-final.tgz')
 
     # plone core
     core   = PLONE_CORE
