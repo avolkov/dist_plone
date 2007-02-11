928db5f5f1a369a387ccd7b453f587f4df81e0f8
Hanno Schlichting <hanno@hannosch.eu>
2007-02-11 20:49:11 +0000

    Added new PloneTranslations release

--- platforms/independent.py
+++ platforms/independent.py
@@ -39,7 +39,7 @@ PLONE_CORE = [
     ZProduct('PlacelessTranslationService',  PLONE_ORG + 'pts/releases/1.4.1/PlacelessTranslationService-1.4.1.tar.gz'),
     ZProduct('PloneErrorReporting',PLONE_ORG + 'ploneerrorreporting/releases/1.1/PloneErrorReporting-1.1.tar.gz'),
     ZProduct('PloneTestCase',PLONE_ORG + 'plonetestcase/releases/0.9.0/PloneTestCase-0.9.0.tar.gz'),
-    ZProduct('PloneTranslations',PLONE_ORG + 'plonetranslations/releases/2.6.1/PloneTranslations-2.6.1.tar.gz'),
+    ZProduct('PloneTranslations',PLONE_ORG + 'plonetranslations/releases/3.0.0/PloneTranslations-3.0.0a2.tar.gz'),
     ZProduct('PloneLanguageTool', PLONE_ORG + 'plonelanguagetool/releases/1.5/PloneLanguageTool-1.5.tar.gz'),
     ZProduct('SecureMailHost', PLONE_ORG + 'securemailhost/releases/1.0.4/SecureMailHost-1.0.4.tar.gz'),
     ZProduct('ResourceRegistries', PLONE_ORG + 'resourceregistries/releases/1.4.0-alpha2/ResourceRegistries-1.4.0-alpha2.tar.gz'),
