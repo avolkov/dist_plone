dad28d75922817dca7c7d87a47eb3fe2e7127bf3
Wichert Akkerman <wichert@wiggy.net>
2007-02-13 16:20:33 +0000

    Use PloneTestCase 0.9.2

--- platforms/independent.py
+++ platforms/independent.py
@@ -38,7 +38,7 @@ PLONE_CORE = [
     ZProduct('GroupUserFolder', PLONE_ORG + 'groupuserfolder/releases/3.54.2/GroupUserFolder-3.54.2.tgz'),
     ZProduct('PlacelessTranslationService',  PLONE_ORG + 'pts/releases/1.4.1/PlacelessTranslationService-1.4.1.tar.gz'),
     ZProduct('PloneErrorReporting',PLONE_ORG + 'ploneerrorreporting/releases/1.1/PloneErrorReporting-1.1.tar.gz'),
-    ZProduct('PloneTestCase',PLONE_ORG + 'plonetestcase/releases/0.9.0/PloneTestCase-0.9.0.tar.gz'),
+    ZProduct('PloneTestCase',PLONE_ORG + 'plonetestcase/releases/0.9.2/PloneTestCase-0.9.2.tar.gz'),
     ZProduct('PloneTranslations',PLONE_ORG + 'plonetranslations/releases/3.0.0/PloneTranslations-3.0.0a2.tar.gz'),
     ZProduct('PloneLanguageTool', PLONE_ORG + 'plonelanguagetool/releases/1.5/PloneLanguageTool-1.5.tar.gz'),
     ZProduct('SecureMailHost', PLONE_ORG + 'securemailhost/releases/1.0.4/SecureMailHost-1.0.4.tar.gz'),
