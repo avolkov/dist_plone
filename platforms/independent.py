98ba491ffc94a0ffbac01330a2ff59184eed1323
Wichert Akkerman <wichert@wiggy.net>
2007-03-09 09:47:51 +0000

    CMF 2.1-beta released

--- platforms/independent.py
+++ platforms/independent.py
@@ -14,8 +14,7 @@ CHEESE_SOURCE = 'http://cheeseshop.python.org/packages/source/'
 
 PLONE_CORE = [
     Bundle('CMF',
-#           'http://www.zope.org/Products/CMF/CMF-2.1.0-alpha2/CMF-2.1.0-alpha2.tar.gz',
-           'http://antiloop.plone.org/download/CMF-2.1-alpha2.1.tar.gz',
+           'http://www.zope.org/Products/CMF/CMF-2.1.0-beta/CMF-2.1.0-beta.tar.gz',
            { 'CMFActionIcons': ZProduct,
              'CMFCalendar': ZProduct,
              'CMFCore'    : ZProduct,
