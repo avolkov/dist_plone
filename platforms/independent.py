c89c943c6b04d80dbfae86d4aacf9634d731cae5
Wichert Akkerman <wichert@wiggy.net>
2007-03-30 13:35:20 +0000

    PlonePAS 2.3 is out with important security and migration bugfixes

--- platforms/independent.py
+++ platforms/independent.py
@@ -41,7 +41,7 @@ PLONE_CORE = [
     ZProduct('SecureMailHost', PLONE_ORG + 'securemailhost/releases/1.0.4/SecureMailHost-1.0.4.tar.gz'),
     ZProduct('ResourceRegistries', PLONE_ORG + 'resourceregistries/releases/1.3.4/ResourceRegistries-1.3.4.tar.gz'),
     ZProduct('statusmessages', PLONE_ORG + 'statusmessages/releases/2.0.1/statusmessages-2.0.1.tar.gz'),
-    ZProduct('PlonePAS', PLONE_ORG + 'plonepas/releases/2.2/PlonePAS-2.2.tar.gz'),
+    ZProduct('PlonePAS', PLONE_ORG + 'plonepas/releases/2.3/PlonePAS-2.3.tar.gz'),
     ZProduct('PluggableAuthService', ZOPE_ORG + 'PluggableAuthService/PluggableAuthService-1.4.1/PluggableAuthService-1.4.1.tar.gz', 'PluggableAuthService-1.4.1'),
     ZProduct('PasswordResetTool', PLONE_ORG + 'passwordresettool/releases/0.4.2/PasswordResetTool-0.4.2.tar.gz'),
     ZProduct('PluginRegistry', ZOPE_ORG + 'PluginRegistry/PluginRegistry-1.1.1/PluginRegistry-1.1.1.tar.gz', 'PluginRegistry-1.1.1'),
