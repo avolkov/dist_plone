63ed859255af4d2ebf8ff40c569a47593afde8ba
Wichert Akkerman <wichert@wiggy.net>
2007-05-02 15:18:29 +0000

    Update to use our CMF snapshot

--- platforms/independent.py
+++ platforms/independent.py
@@ -14,7 +14,8 @@ CHEESE_SOURCE = 'http://cheeseshop.python.org/packages/source/'
 
 PLONE_CORE = [
     Bundle('CMF',
-           'http://www.zope.org/Products/CMF/CMF-2.1.0-beta/CMF-2.1.0-beta.tar.gz',
+#           'http://www.zope.org/Products/CMF/CMF-2.1.0-beta/CMF-2.1.0-beta.tar.gz',
+           'http://antiloop.plone.org/download/CMF-2.1.0-beta-plone-3.0b2.tar.gz',
            { 'CMFActionIcons': ZProduct,
              'CMFCalendar': ZProduct,
              'CMFCore'    : ZProduct,
