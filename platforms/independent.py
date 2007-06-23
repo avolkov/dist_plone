78fae759654bd96045169405699fe8b20bd01d68
Hanno Schlichting <hanno@hannosch.eu>
2007-06-23 12:16:39 +0000

    Updated to new five.lsm version which sets the zip-safe-flag right

--- platforms/independent.py
+++ platforms/independent.py
@@ -87,7 +87,7 @@ PLONE_CORE_PACKAGES = [
     PyModule('plone.session', CHEESE_SOURCE + 'p/plone.session/plone.session-1.0b2.tar.gz', version="1.0b2"),
     PyModule('wicked', CHEESE_SOURCE + 'w/wicked/wicked-1.1.1.tar.gz', version="1.1.2"),
     PyModule('five.customerize', CHEESE_SOURCE + 'f/five.customerize/five.customerize-0.1.2.tar.gz', version="0.1.2"),
-    PyModule('five.localsitemanager', CHEESE_SOURCE + 'f/five.localsitemanager/five.localsitemanager-0.1.1.tar.gz', version="0.1.1"),
+    PyModule('five.localsitemanager', CHEESE_SOURCE + 'f/five.localsitemanager/five.localsitemanager-0.1.2.tar.gz', version="0.1.2"),
 ]
 
 
