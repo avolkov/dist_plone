78814280f36806bfaef1981552099b0e1e164527
Daniel Nouri <daniel.nouri@gmail.com>
2007-07-04 20:03:31 +0000

    Update to AT 1.5 rc1

--- platforms/independent.py
+++ platforms/independent.py
@@ -95,7 +95,7 @@ ADDONS = [
 ]
 
 AT1_5 = [
-    ZProduct('Archetypes', PLONE_ORG + 'archetypes/releases/1.5/Archetypes-1.5.0-b5.tar.gz'),
+    ZProduct('Archetypes', PLONE_ORG + 'archetypes/releases/1.5/Archetypes-1.5.0-rc1.tar.gz'),
 ]
 
 README_TXT = """Plone's README is in CMFPlone/README.txt
