94f4a84ae24f30718e23221103475591d634866f
Wichert Akkerman <wichert@wiggy.net>
2007-07-13 07:59:16 +0000

    Zope 2.10.4 is there

--- platforms/independent.py
+++ platforms/independent.py
@@ -106,7 +106,7 @@ class Distribution:
 
     # this is what plone is based on
     python =  Software('python', 'http://www.python.org/ftp/python/2.4.4/Python-2.4.4.tgz')
-    zope   =  Software('zope'  , ZOPE_ORG + 'Zope/2.10.3/Zope-2.10.3-final.tgz')
+    zope   =  Software('zope'  , ZOPE_ORG + 'Zope/2.10.4/Zope-2.10.4-final.tgz')
 
     # plone core
     core   = PLONE_CORE + AT1_5
