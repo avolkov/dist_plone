12bbf274d3c9a85d9af3e386639c8f8f2b42badb
Wichert Akkerman <wichert@wiggy.net>
2007-09-11 16:59:48 +0000

    PAS 1.5.1

--- platforms/independent.py
+++ platforms/independent.py
@@ -47,7 +47,7 @@ PLONE_CORE = [
     ZProduct('ResourceRegistries', PLONE_ORG + 'resourceregistries/releases/1.4.1/resourceregistries-1-4-1.tgz'),
     ZProduct('statusmessages', PLONE_ORG + 'statusmessages/releases/3.0/statusmessages-3.0.tar.gz'),
     ZProduct('PlonePAS', PLONE_ORG + 'plonepas/releases/3.0/PlonePAS-3.0.tar.gz'),
-    ZProduct('PluggableAuthService', ZOPE_ORG + 'PluggableAuthService/PluggableAuthService-1.5/PluggableAuthService-1.5.tar.gz', 'PluggableAuthService-1.5'),
+    ZProduct('PluggableAuthService', ZOPE_ORG + 'PluggableAuthService/PluggableAuthService-1.5/PluggableAuthService-1.5.1.tar.gz', 'PluggableAuthService-1.5.1'),
     ZProduct('PasswordResetTool', PLONE_ORG + 'passwordresettool/releases/1.0/PasswordResetTool-1.0.tar.gz'),
     ZProduct('PluginRegistry', ZOPE_ORG + 'PluginRegistry/PluginRegistry-1.1.2/PluginRegistry-1.1.2.tar.gz', 'PluginRegistry-1.1.2'),
     ZProduct('ZopeVersionControl', 'http://antiloop.plone.org/download/ZopeVersionControl-0.3.4.tar.gz'),
