57e481255be400b48932fc09f538720245c9783e
Alec Mitchell <alecpm@gmail.com>
2007-09-11 05:43:40 +0000

    Update ATCT release

--- platforms/independent.py
+++ platforms/independent.py
@@ -26,7 +26,7 @@ PLONE_CORE = [
              'GenericSetup' : ZProduct,
            }
     ),
-    ZProduct('ATContentTypes', PLONE_ORG + 'atcontenttypes/releases/1.2.1/ATContentTypes-1.2.1.tgz'),
+    ZProduct('ATContentTypes', PLONE_ORG + 'atcontenttypes/releases/1.2.1/ATContentTypes-1.2.1-2.tgz'),
     ZProduct('ATReferenceBrowserWidget', PLONE_ORG + 'atreferencebrowserwidget/releases/2.0.1/ATReferenceBrowserWidget-2.0.1.tar.gz'),
     ZProduct('CMFDynamicViewFTI', PLONE_ORG + 'cmfdynamicviewfti/releases/3.0/CMFDynamicViewFTI-3.0.tar.gz'),
     ZProduct('CMFFormController', PLONE_ORG + 'cmfformcontroller/releases/2.1/CMFFormController-2.1.tar.gz'),
