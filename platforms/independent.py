dbf2027e2d237c42184de6f6fd5b3b967daab0e6
Wichert Akkerman <wichert@wiggy.net>
2007-09-13 09:08:28 +0000

    PSC name wrangling strikes again...

--- platforms/independent.py
+++ platforms/independent.py
@@ -26,7 +26,7 @@ PLONE_CORE = [
            }
     ),
     ZProduct('GenericSetup', ZOPE_ORG + 'GenericSetup/GenericSetup-1.3.2/GenericSetup-1.3.2.tar.gz'),
-    ZProduct('ATContentTypes', PLONE_ORG + 'atcontenttypes/releases/1.2.1/atcontenttypes-1.2.1-3.tgz'),
+    ZProduct('ATContentTypes', PLONE_ORG + 'atcontenttypes/releases/1.2.1/ATContentTypes-1.2.1-3.tgz'),
     ZProduct('ATReferenceBrowserWidget', PLONE_ORG + 'atreferencebrowserwidget/releases/2.0.1/ATReferenceBrowserWidget-2.0.1.tar.gz'),
     ZProduct('CMFDynamicViewFTI', PLONE_ORG + 'cmfdynamicviewfti/releases/3.0/CMFDynamicViewFTI-3.0.tar.gz'),
     ZProduct('CMFFormController', PLONE_ORG + 'cmfformcontroller/releases/2.1/CMFFormController-2.1.tar.gz'),
