7112b16be1dd2b9ea67afce9104dc908e21a61ea
Hanno Schlichting <hanno@hannosch.eu>
2007-12-24 11:38:17 +0000

    More releases.

--- platforms/independent.py
+++ platforms/independent.py
@@ -39,7 +39,7 @@ PLONE_CORE = [
     ZProduct('ExternalEditor', PLONE_ORG + 'external-editor/releases/0.9.3/ExternalEditor-0.9.3-src.tgz'),
     ZProduct('GroupUserFolder', PLONE_ORG + 'groupuserfolder/releases/3.55.1/groupuserfolder-3-55-1.tgz'),
     ZProduct('kupu', PLONE_ORG + 'kupu/releases/1.4.6/kupu-1-4-6.tgz'),
-    ZProduct('PlacelessTranslationService',  PLONE_ORG + 'pts/releases/1.4.6/PlacelessTranslationService-1.4.6.tar.gz'),
+    ZProduct('PlacelessTranslationService',  PLONE_ORG + 'pts/releases/1.4.7/PlacelessTranslationService-1.4.7.tar.gz'),
     ZProduct('PloneTestCase',PLONE_ORG + 'plonetestcase/releases/0.9.7/PloneTestCase-0.9.7.tar.gz'),
     ZProduct('PloneTranslations', PLONE_ORG + 'plonetranslations/releases/3.0.9/PloneTranslations-3.0.9.tar.gz'),
     ZProduct('PloneLanguageTool', PLONE_ORG + 'plonelanguagetool/releases/2.0.1/PloneLanguageTool-2.0.1.tar.gz'),
@@ -74,7 +74,7 @@ PLONE_CORE_PACKAGES = [
     PyModule('plone.app.portlets', CHEESE_SOURCE + 'p/plone.app.portlets/plone.app.portlets-1.0.3.tar.gz', version="1.0.3"),
     PyModule('plone.app.redirector', CHEESE_SOURCE + 'p/plone.app.redirector/plone.app.redirector-1.0.2.tar.gz', version="1.0.2"),
     PyModule('plone.app.viewletmanager', CHEESE_SOURCE + 'p/plone.app.viewletmanager/plone.app.viewletmanager-1.0.tar.gz', version="1.0"),
-    PyModule('plone.app.vocabularies', CHEESE_SOURCE + 'p/plone.app.vocabularies/plone.app.vocabularies-1.0.1.tar.gz', version="1.0.1"),
+    PyModule('plone.app.vocabularies', CHEESE_SOURCE + 'p/plone.app.vocabularies/plone.app.vocabularies-1.0.2.tar.gz', version="1.0.2"),
     PyModule('plone.app.workflow', CHEESE_SOURCE + 'p/plone.app.workflow/plone.app.workflow-1.0.1.1.tar.gz', version="1.0.1.1"),
     PyModule('plone.contentrules', CHEESE_SOURCE + 'p/plone.contentrules/plone.contentrules-1.0.1.1.tar.gz', version="1.0.1.1"),
     PyModule('plone.fieldsets', CHEESE_SOURCE + 'p/plone.fieldsets/plone.fieldsets-1.0.tar.gz', version="1.0"),
@@ -88,7 +88,7 @@ PLONE_CORE_PACKAGES = [
     PyModule('plone.theme', CHEESE_SOURCE + 'p/plone.theme/plone.theme-1.0.tar.gz', version="1.0"),
     PyModule('wicked', CHEESE_SOURCE + 'w/wicked/wicked-1.1.6.tar.gz', version="1.1.6"),
     PyModule('five.customerize', CHEESE_SOURCE + 'f/five.customerize/five.customerize-0.2.tar.gz', version="0.2"),
-    PyModule('five.localsitemanager', CHEESE_SOURCE + 'f/five.localsitemanager/five.localsitemanager-0.2.tar.gz', version="0.2"),
+    PyModule('five.localsitemanager', CHEESE_SOURCE + 'f/five.localsitemanager/five.localsitemanager-0.3.tar.gz', version="0.3"),
 ]
 
 
