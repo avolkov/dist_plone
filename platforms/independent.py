95c49c811013913600536f0d357c7d3fcea66627
Wichert Akkerman <wichert@wiggy.net>
2008-03-08 11:58:45 +0000

    GenericSetup 1.4.0beta

--- platforms/independent.py
+++ platforms/independent.py
@@ -23,9 +23,9 @@ PLONE_CORE = [
              'CMFTopic'   : ZProduct,
              'CMFUid'     : ZProduct,
              'DCWorkflow' : ZProduct,
-             'GenericSetup': ZProduct,
            }
     ),
+    ZProduct('GenericSetup', ZOPE_ORG + 'GenericSetup/GenericSetup-1.4.0-beta/GenericSetup-1.4.0-beta.tar.gz'),
     ZProduct('ATContentTypes', PLONE_GOOGLE + 'ATContentTypes-1.2.4.tgz'),
     ZProduct('ATReferenceBrowserWidget', PLONE_ORG + 'atreferencebrowserwidget/releases/2.0.1/ATReferenceBrowserWidget-2.0.1.tar.gz'),
     ZProduct('CMFDynamicViewFTI', PLONE_ORG + 'cmfdynamicviewfti/releases/3.0.1/CMFDynamicViewFTI-3.0.1.tar.gz'),
