2b316bf0bca8cc8aabe8d0c87149f9c224dd696c
Wichert Akkerman <wichert@wiggy.net>
2008-03-27 22:28:45 +0000

    Correct URL for passwordresettool

--- platforms/independent.py
+++ platforms/independent.py
@@ -48,7 +48,7 @@ PLONE_CORE = [
     ZProduct('statusmessages', PLONE_ORG + 'statusmessages/releases/3.0.3/statusmessages-3.0.3-tar.gz'),
     ZProduct('PlonePAS', PLONE_ORG + 'plonepas/releases/3.4/PlonePAS-3.4.tar.gz'),
     ZProduct('PluggableAuthService', PLONE_DIST + 'PluggableAuthService-1.5.3.tar.gz'),
-    ZProduct('PasswordResetTool', PLONE_ORG + 'passwordresettool/releases/1.1/PasswordResetTool-1.1.tar.gz'),
+    ZProduct('PasswordResetTool', PLONE_ORG + 'passwordresettool/releases/1.1/passwordresettool-1.1.tar.gz'),
     ZProduct('PluginRegistry', ZOPE_ORG + 'PluginRegistry/PluginRegistry-1.1.2/PluginRegistry-1.1.2.tar.gz', 'PluginRegistry-1.1.2'),
     ZProduct('ZopeVersionControl', 'http://antiloop.plone.org/download/ZopeVersionControl-0.3.4.tar.gz'),
     ZProduct('CMFEditions', PLONE_ORG + 'cmfeditions/releases/1.1.6/CMFEditions-1.1.6.tgz'),
