8a9f6d56228d81999f1963c7cbeca0405e312a2d
Wichert Akkerman <wichert@wiggy.net>
2008-04-28 12:49:32 +0000

    Use the updated AT release archive

--- platforms/independent.py
+++ platforms/independent.py
@@ -103,7 +103,7 @@ ADDONS = [
 ]
 
 AT1_5 = [
-    ZProduct('Archetypes', PLONE_ORG + 'archetypes/releases/1.5/Archetypes-1.5.8.tgz'),
+    ZProduct('Archetypes', PLONE_ORG + 'archetypes/releases/1.5/Archetypes-1.5.8-2.tar.gz'),
 ]
 
 README_TXT = """Plone's README is in CMFPlone/README.txt
